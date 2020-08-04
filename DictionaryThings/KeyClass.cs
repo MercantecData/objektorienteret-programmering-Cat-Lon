01bcac6dec48d77f2e63fc7e244c7e47dde0d82b
william <william_kammer@live.dk>
2020-08-04 09:00:16 +0200

    Dictionary opgave+ bonus
    Hvis jeg ikke tager fejl er der også andre ting men er dog ikke sikker

---
+++ DictionaryThings/KeyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryTest
+{
+    public class KeyClass
+    {
+        public string navn = "Joe";
+        public int alder = 69;
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using PersonLib;
+using DictionaryTest;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryThings InitializedDictionary = new DictionaryThings();
+            Console.WriteLine(InitializedDictionary.kFloat[1]); 
+            Dictionary<KeyClass, string> bonusDict = new Dictionary<KeyClass, string>();
+        }
+        static void PersonLibTest()
+        {
+            Job janitor = new Job(100, "janitor", true);
+            Housing housing1 = new Housing(2000, "123 gadeallé", true);
+
+            Person person1 = new Person("Bill", 31, janitor, housing1);
+            Person person2 = new Person("Bob", 19, janitor, housing1);
+
+            PersonList personList = new PersonList();
+
+            personList.AddPerson(person1);
+            personList.AddPerson(person2);
+
+            foreach (Person element in personList.personList)
+            {
+                Console.WriteLine(element.name + " " + element.age + " " + element.work.name);
+            }
+        }
+    }
+}
